8c6cfc5839caf2becf8c1fa27007653fed6572a8
teerth-45 <71254478+teerth-45@users.noreply.github.com>
2020-12-09 21:55:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var rec1, rec2, rec3;
+var paperObject, groundObject, dustbinObject;
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	paperObject= new paper(200,450,40);
+	groundObject= new ground(width/2,670,width,20);
+	dustbinObject=new dustbin(1200,650);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+  
+  ///paperObject.collide(groundSprite);
+
+  drawSprites();
+  
+  dustbinObject.display();
+  paperObject.display();
+  groundObject.display();
+  
+ 
+}
+
+function keyPressed(){
+	if (keyCode===UP_ARROW){
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:90,y:-90});
+	}
+}
+
+
+
